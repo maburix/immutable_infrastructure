b10dda89a435eaa10ea030011eee023fa215c935
Fernando Ike <fike@maburix.com>
2018-11-11 23:59:54 -0200

    Fixed lint messages

--- ansible/molecule/default/tests/test_default.py
+++ ansible/molecule/default/tests/test_default.py
@@ -8,8 +8,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_hello_running_and_enabled(host):
     service = host.service('hello')
-    
-    assert  service.is_running
+
+    assert service.is_running
 
 
 def test_flask_listening_http(host):
